fa2b227a47c890c8809e1f468d34f2fdde6ef4c7
loitrem <ski_fix@hotmail.com>
2023-07-23 15:03:39 -0400

    basic functionality works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  let wordTyped='';
+  const [data, setData] = useState(null);
+  console.log(data);
+  const getData = async (word) => {
+    wordTyped = word;
+    let response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    let info = await response.data;
+    console.log(response);
+    setData(info[0]);
+
+}
+
+useEffect(()=> {
+  getData();
+},[])
+const loaded = () => {
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="container">
+          <div className="search-box">
+            <input type="text" placeholder="Type the word here..." id="inp-word" />
+            <button id="search-btn" onClick={() => {
+              getData(document.getElementById('inp-word').value)
+              }}>Search</button>
+          </div>
+          <div id="result" className="result">
+            <div className="word"><h3>{wordTyped}</h3></div>
+            <div className="details">
+              <p>{data.meanings[0].partOfSpeech}</p>
+              <p>{data.phonetic}/</p>
+            </div>
+            <p className="word-meaning">
+              {data.meanings[0].definitions[0].definition}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    );
+  }
+
+          // Function for when data doesn't exist
+          const loading = () => {
+            return <h1>Loading...</h1>;
+        };
+        
+        // if coin has data, run the loaded function, otherwise, run loading
+        return data && data.word ? loaded() : loading();
 }
 
 export default App;
